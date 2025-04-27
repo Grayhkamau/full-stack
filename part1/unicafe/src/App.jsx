00303faee725167ca2c91a75041e29fa84bea871
Gray <hkamau263@gmail.com>
2025-04-27 12:57:23 +0300

    unicafe - excersice 1.12

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,6 +10,18 @@ const App = ()=>{
     const [average,setAverage] = useState(0);
     const [all,setAll] = useState(0)
     const [positive, setPositive] = useState(0);
+    const [anecdote, setAnecdote] = useState(0);
+    const anecdotes = [
+        'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+    ];
+
 
     const handleGoodClick = ()=> {
         setGood(good+1)
@@ -30,10 +42,15 @@ const App = ()=>{
         setPositive((good/(all+1))*100)
 
     }
- 
+    const changeAnecdote = ()=>{
+        setAnecdote(Math.floor(Math.random()*anecdotes.length))
+    }
 
     return(
         <div>
+            <p>{anecdotes[anecdote]}</p>
+            <Buttons text={"next anecdotes"} onClick={changeAnecdote}/>
+
             <h1>give Feedback</h1>
 
             <Buttons text={"good"} onClick={handleGoodClick}/>
@@ -44,7 +61,7 @@ const App = ()=>{
 
             {!all?<h4>No feedback given</h4>:
                 <>
-                
+
                     <StatisticsLine text={"good"} value={good}/>
                     <StatisticsLine text={"neutral"} value={neutral}/>
                     <StatisticsLine text={"bad"} value={bad}/>
