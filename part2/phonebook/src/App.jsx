ac55d1fcf8a7386245d7864cb06e75cf5672d013
Gray <hkamau263@gmail.com>
2025-04-29 14:41:46 +0300

    part2 phonebook - 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,19 +5,45 @@ const App=()=>{
     {name:'Arto Hellas'}
   ])
   const [newName, setNewName] = useState('');
+  const handleNewName = (e)=>{
+    e.preventDefault();
+    if(!newName) return;
 
+    let exists = persons.findIndex(person=>{
+      return person.name===newName
+    })
+
+
+    if(exists>=0) {
+      setNewName("")
+      return alert(`${newName} is already added to the phonebook`)
+    }
+    console.log('reaching before')
+
+    setPersons([...persons, {name:newName}])
+    setNewName("")
+  }
+  // console.log(persons)
+ 
   return(
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={(handleNewName)}>
         <div>
-          name: <input/>
+
+          name: <input 
+          value={newName} 
+          onChange={(e)=>setNewName(e.target.value)}/>
+
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>numbers</h2>
+      {persons.map((person,index)=>{
+        return <p key={index}>{person.name}</p>
+      })}
     </div>
   )
 }
