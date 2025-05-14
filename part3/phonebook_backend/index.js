6a9af112d14b6dcc7cb119009578b37f8182055d
Gray <hkamau263@gmail.com>
2025-05-14 22:09:47 +0300

    part3 phonebook_backend 3.22

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,88 +1,88 @@
-require("dotenv").config();
-const express = require('express');
-const logger = require('morgan');
-const PhoneBookModel = require('./models/phonebook');
+require('dotenv').config()
+const express = require('express')
+const logger = require('morgan')
+const PhoneBookModel = require('./models/phonebook')
 
-const app = express();
-app.use(express.static("dist"));
-app.use(express.json());
+const app = express()
+app.use(express.static('dist'))
+app.use(express.json())
 app.use(logger(':method :url :status :res[content-length] - :response-time ms :response'))
 
 
-logger.token('response',function(req,res){
-    return JSON.stringify(req.body)
+logger.token('response',function(req){
+  return JSON.stringify(req.body)
 })
 
 
-app.get('/api/persons',(req,res)=>{
-    PhoneBookModel.find({}).then((result)=>{
-        res.json(result||[])
+app.get('/api/persons',(req,res) => {
+  PhoneBookModel.find({}).then((result) => {
+    res.json(result||[])
+  })
+})
+
+app.get('/info',(req,res) => {
+  PhoneBookModel.find()
+    .then(people => {
+      res.send(`<div><p> Phonebook has info for ${people.length} people</p> <p>${Date()}</p></div>`)
     })
 })
 
-app.get('/info',(req,res)=>{
-    PhoneBookModel.find()
-    .then(people=>{
-        res.send(`<div><p> Phonebook has info for ${people.length} people</p> <p>${Date()}</p></div>`)
+app.get('/api/persons/:id', (req,res,next) => {
+  const id = req.params.id
+  PhoneBookModel.findById(id)
+    .then(person => {
+      if(!person) res.status(404).end()
+      res.json(person)
     })
-})
-
-app.get('/api/persons/:id', (req,res,next)=>{
-    const id = req.params.id;
-    PhoneBookModel.findById(id)
-    .then(person=>{
-        if(!person) res.status(404).end()
-        res.json(person)
-    })
-    .catch(err=>next(err))
+    .catch(err => next(err))
 })
 
 //implemented deleting people in the database and intregrated with frontend
-app.delete('/api/persons/:id',(req,res, next)=>{
-    const id = req.params.id
-    
-    PhoneBookModel.findByIdAndDelete(id)
-    .then(personRemoved=>res.json({id:personRemoved.id}).status(200))
-    .catch(err=>next(err))
+app.delete('/api/persons/:id',(req,res, next) => {
+  const id = req.params.id
+
+  PhoneBookModel.findByIdAndDelete(id)
+    .then(personRemoved => res.json({ id:personRemoved.id }).status(200))
+    .catch(err => next(err))
 
 })
 
-app.post('/api/persons', (req,res, next)=>{
-    let body = req.body;    
+app.post('/api/persons', (req,res, next) => {
+  let body = req.body
 
-        let newPerson = new PhoneBookModel({
-            name:body.name,
-            number:body.number
-        })
-        newPerson.save().then(person=>{
-            res.json(person)
-        }) 
-        .catch(error=>next(error))  
+  let newPerson = new PhoneBookModel({
+    name:body.name,
+    number:body.number
+  })
+  newPerson.save().then(person => {
+    res.json(person)
+  })
+    .catch(error => next(error))
 })
 
 
-app.put('/api/persons/:id', (req,res,next)=>{
-    let id = req.params.id;
-    let {number, name} = req.body;
-    PhoneBookModel.findByIdAndUpdate(id, {number})
-    .then(updatedPerson=>{
-        if(!updatedPerson) res.status(404).end()
-        updatedPerson.number = number
-        res.json(updatedPerson)
+app.put('/api/persons/:id', (req,res,next) => {
+  let id = req.params.id
+  let { number } = req.body
+  PhoneBookModel.findByIdAndUpdate(id, { number })
+    .then(updatedPerson => {
+      if(!updatedPerson) res.status(404).end()
+      updatedPerson.number = number
+      res.json(updatedPerson)
     })
-    .catch(err=>next(err))
+    .catch(err => next(err))
 })
 
-const errorHandler = (error,req,res,next)=>{
-        if(error.name === "CastError") res.status(400).send({error:'malformatted id'})
-        else if(error.name="ValidationError") res.status(400).json({error:error.message})
-        next(error);
+const errorHandler = (error,req,res,next) => {
+  if(error.name === 'CastError') res.status(400).send({ error:'malformatted id' })
+  else if(error.name==='ValidationError') res.status(400).json({ error:error.message })
+  next(error)
 }
 
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = 3001;
-app.listen(PORT,()=>{
-    console.log(`app listening on port ${PORT}`)
+const PORT = 3001
+app.listen(PORT,() => {
+  console.log(`app listening on port ${PORT}`)
 })
--- part3/phonebook_backend/models/phonebook.js
+++ part3/phonebook_backend/models/phonebook.js
@@ -1,40 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 
-mongoose.set("strictQuery", false)
+mongoose.set('strictQuery', false)
 
-const url =  process.env.MONGODB_URL;
+const url =  process.env.MONGODB_URL
 
 mongoose.connect(url)
-.then(()=>console.log('db connected'))
-.catch((err)=>console.log('error ocurred', err.message))
+  .then(() => console.log('db connected'))
+  .catch((err) => console.log('error ocurred', err.message))
 
 const phoneBookSchema = new mongoose.Schema({
-        name:{
-            type:String,
-            minlength: 3,
-            required:true
-        },
-       
-        number:{
-            type:String,
-            minlength:8,
-            required:true,
-            validate:[function (number){
-                return (number.includes('-') && number.split("-").length===2 && (number.split("-")[0].length==3 || number.split("-")[0].length===2))
-            }, "invalid phone number"]
-        }
+  name:{
+    type:String,
+    minlength: 3,
+    required:true
+  },
+
+  number:{
+    type:String,
+    minlength:8,
+    required:true,
+    validate:[function (number){
+      return (number.includes('-') && number.split('-').length===2 && (number.split('-')[0].length===3 || number.split('-')[0].length===2))
+    }, 'invalid phone number']
+  }
 })
 
 phoneBookSchema.set('toJSON',{
-    transform:(document,returnedObject)=>{
-        returnedObject.id = returnedObject._id.toString()
+  transform:(document,returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
 
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
-const PhoneBookModel = mongoose.model('person',phoneBookSchema);
+const PhoneBookModel = mongoose.model('person',phoneBookSchema)
 
 
-module.exports = PhoneBookModel;
+module.exports = PhoneBookModel
