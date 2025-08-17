d3be374774cfc5a0013000b55930c9ef1b2e2ec4
Gray <hkamau263@gmail.com>
2025-08-17 19:44:39 +0300

    part4 blog-list 4.6

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -1,6 +1,6 @@
 const {test,describe} = require('node:test');
 const assert = require('node:assert');
-const {dummy,total_likes, favouriteBlog} =  require('../utils/list_helper');
+const {dummy,total_likes, favouriteBlog, mostBlogs} =  require('../utils/list_helper');
 
 
 test('dummy', ()=>{
@@ -77,7 +77,7 @@ describe("favourite blog", ()=>{
 
         assert.deepStrictEqual(favouriteBlog(blog_list),blog_list[0])
     })
-     test('of a bigger list favourite is calculated right', ()=>{
+    test('of a bigger list favourite is calculated right', ()=>{
         let blog_list = [{
                             _id: '5a422aa71b54a676234d17f8',
                             title: 'Go To Statement Considered Harmful',
@@ -103,5 +103,57 @@ describe("favourite blog", ()=>{
                             __v: 0
                         }]
         assert.deepStrictEqual(favouriteBlog(blog_list),blog_list[blog_list.length-1])
+    })
+})
+
+
+describe("most blogs", ()=>{
+    test("of empty blogs is zero", ()=>{
+
+        let blog_list = []
+
+        assert.strictEqual(mostBlogs(blog_list),0)
+    })
+
+    test("when list has one blog equals that blog", ()=>{
+        let blog_list = [
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+                likes: 5,
+                __v: 0
+            }]
+
+        assert.deepStrictEqual(mostBlogs(blog_list),{author:'Edsger W. Dijkstra',blogs:1})
+    })
+    test('of a bigger list author with most blogs is calculated right',()=>{
+        let blog_list = [{
+                            _id: '5a422aa71b54a676234d17f8',
+                            title: 'Go To Statement Considered Harmful',
+                            author: 'kamau',
+                            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+                            likes: 10,
+                            __v: 0
+                        },
+                        {
+                            _id: '5a422aa71b54a676234d17f8',
+                            title: 'Go To Statement Considered Harmful',
+                            author: 'Edsger W. Dijkstra',
+                            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+                            likes: 5,
+                            __v: 0
+                        },
+                        {
+                            _id: '5a422aa71b54a676234d17f8',
+                            title: 'Go To Statement Considered Harmful',
+                            author: 'kamau',
+                            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+                            likes: 25,
+                            __v: 0
+                        }]
+
+        assert.deepStrictEqual(mostBlogs(blog_list),{author:'kamau',blogs:2})
     })
 })
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -21,4 +21,28 @@ const favouriteBlog = (blogs)=>{
 }
     
 
-module.exports = {dummy,total_likes, favouriteBlog};
+const mostBlogs = (blogs)=>{
+    
+    if(!blogs.length) return 0;
+
+    let authorsAndBlogsReducer = (authorsAndBlogsArray, blog)=>{
+        let authorAndBlogInArr = authorsAndBlogsArray.find(authorAndBlog=>authorAndBlog.author === blog.author)
+        if(authorAndBlogInArr) {
+            authorAndBlogInArr.blogs++
+            return authorsAndBlogsArray
+        }
+        authorsAndBlogsArray.push({author:blog.author,blogs:1})
+        return authorsAndBlogsArray
+    }
+
+    let authorsAndBlogs = blogs.reduce(authorsAndBlogsReducer, []);
+
+    let mostBlogsReducer = (topAuthor, currentAuthor)=>{
+        if(topAuthor.blogs>currentAuthor.blogs) return topAuthor
+
+        return currentAuthor
+    }
+
+    return authorsAndBlogs.reduce(mostBlogsReducer,authorsAndBlogs[0]) 
+}
+module.exports = {dummy,total_likes, favouriteBlog, mostBlogs};
