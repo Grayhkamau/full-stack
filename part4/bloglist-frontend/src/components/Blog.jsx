b218a906f3c8991c020bdcfccde9e4f803194480
Gray <hkamau263@gmail.com>
2025-08-25 00:00:29 +0300

    part5 blog-list-frontend 5.7

--- part4/bloglist-frontend/src/components/Blog.jsx
+++ part4/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,32 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+const Blog = ({ blog }) => {
+  const [showDetails,setShowDetails] = useState(false)
+  
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  
+  return(
+    <div style={blogStyle}>
+      <p>{blog.title} <button onClick={()=>setShowDetails(!showDetails)}>{showDetails?"hide":"view"}</button></p>
+      {setShowDetails
+      ?
+        <>
+          <p>{blog.url}</p>
+          <p>likes: {blog.likes} <button>like</button></p>
+          <p>{blog.author}</p>
+        </> 
+      :
+      ''
+      }
+    </div> 
+  )
+   
+}
 
 export default Blog
