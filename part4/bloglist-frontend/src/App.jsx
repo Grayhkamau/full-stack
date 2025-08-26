105cefafeb0937ecdd284838371204a365e0a8b6
Gray <hkamau263@gmail.com>
2025-08-26 14:50:00 +0300

    part5 blog-list-frontend 5.10

--- part4/bloglist-frontend/src/App.jsx
+++ part4/bloglist-frontend/src/App.jsx
@@ -124,7 +124,8 @@ const handleUpdateLike = async(blogId)=>{
             <AddBlogForm  submitBlog={submitBlog}/>
           </Toggable>
           {
-          blogs.map(blog =>
+            
+          [...blogs].sort((a,b)=>b.likes-a.likes).map(blog =>
             <Blog key={blog.id} blog={blog}  handleUpdateLike={handleUpdateLike}/>
           )}
         </>
